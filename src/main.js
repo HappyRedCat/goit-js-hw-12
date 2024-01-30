c7f7d288c0ada7f96904ba4969499d734c837e40
HappyRedCat <HappyRedCat@users.noreply.github.com>
2024-01-30 20:28:54 +0100

    up

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 'use strict';
-
 import axios from 'axios';
 // Описаний у документації
 import iziToast from 'izitoast';
@@ -33,62 +32,63 @@ loadMoreButton.addEventListener('click', loadMoreImages);
 async function handleFormSubmit(event) {
   event.preventDefault();
   searchQuery = event.target.querySelector('.search-input').value.trim();
-if (!searchQuery) {
-  showWarningToast('Please enter image name!');
-  return;
-}
+  if (!searchQuery) {
+    showWarningToast('Please enter image name!');
+    return;
+  }
 
-clearGallery();
-showLoader();
-hideLoadMoreButton();
+  clearGallery();
+  showLoader();
+  hideLoadMoreButton();
 
-try {
-  const response = await axios.get('https://pixabay.com/api/', {
-    params: {
-      key: API_KEY,
-      per_page: 40,
-      safesearch: true,
-      page: currentPage,
-      orientation: 'horizontal',
-      image_type: 'photo',
-      q: searchQuery,
-    },
-  });
-hideLoader();
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key: API_KEY,
+        per_page: 40,
+        safesearch: true,
+        page: currentPage,
+        orientation: 'horizontal',
+        image_type: 'photo',
+        q: searchQuery,
+      },
+    });
+    hideLoader();
 
-const data = response.data;
-if (data.hits.length === 0) {
-  showErrorToast('Sorry, there are no images matching your search query. Please try again!');
-  return;
-}
+    const data = response.data;
+    if (data.hits.length === 0) {
+      showErrorToast(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+      return;
+    }
 
-totalHits = data.totalHits;
-imagesLoaded = data.hits.length;
-renderImages(data.hits);
-lightbox.refresh();
+    totalHits = data.totalHits;
+    imagesLoaded = data.hits.length;
+    renderImages(data.hits);
+    lightbox.refresh();
 
-showInfoToast(`We found ${totalHits} images.`);
+    showInfoToast(`We found ${totalHits} images.`);
 
-if (totalHits > 40) {
-  showLoadMoreButton();
-}
-
-} catch (error) {
-  console.error('Error fetching images:', error);
-  hideLoader();
-  showErrorToast('Failed to fetch images. Please try again later.');
-}
+    if (totalHits > 40) {
+      showLoadMoreButton();
+    }
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    hideLoader();
+    showErrorToast('Failed to fetch images. Please try again later.');
+  }
 }
 
 function renderImages(images) {
   const fragment = document.createDocumentFragment();
 
-images.forEach(image => {
-  const imageCardElement = createImageCard(image);
-  fragment.appendChild(imageCardElement);
-});
+  images.forEach(image => {
+    const imageCardElement = createImageCard(image);
+    fragment.appendChild(imageCardElement);
+  });
 
-gallery.appendChild(fragment);
+  gallery.appendChild(fragment);
 }
 
 function createImageCard(image) {
@@ -132,7 +132,7 @@ async function loadMoreImages() {
         q: searchQuery,
       },
     });
-    
+
     hideLoader();
 
     const data = response.data;
@@ -153,7 +153,9 @@ async function loadMoreImages() {
 
     if (imagesLoaded >= totalHits) {
       hideLoadMoreButton();
-      showInfoToast(`We're sorry, but you've reached the end of search results.`);
+      showInfoToast(
+        `We're sorry, but you've reached the end of search results.`
+      );
     } else {
       showLoadMoreButton();
     }
@@ -187,7 +189,7 @@ const showLoadMoreButton = () => {
 const hideLoadMoreButton = () => {
   loadMoreButton.style.display = 'none';
 };
-const showWarningToast = (message) => {
+const showWarningToast = message => {
   iziToast.warning({
     title: 'Warning!',
     message: message,
@@ -195,14 +197,14 @@ const showWarningToast = (message) => {
   });
 };
 
-const showErrorToast = (message) => {
+const showErrorToast = message => {
   iziToast.error({
     message: message,
     position: 'topRight',
   });
 };
 
-const showInfoToast = (message) => {
+const showInfoToast = message => {
   iziToast.info({
     message: message,
     position: 'topRight',
